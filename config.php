93214405130d5af68ef71bebf305f56604fbcd96
simoncode12 <123048201+simoncode12@users.noreply.github.com>
2025-08-03 17:56:16 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+       
+        return array(
+            'db_host'      => 'localhost',
+            'db_user'      => 'user_adflex',
+            'db_pass'      => 'Puputchen12$',
+            'db_name'      => 'user_adflex',
+            'install_date' => '1754217308',
+            'appkey'       => '2eee84e662d1667c815945bd7d59e29c9120a2cc',
+            'purchase_code' => '3c8003fd-fb13-4e56-8abe-d5d20b299d84',
+        );
